72941fa0d37925b3b76566155612d9ff7b3d336a
Emir <emirkan.ulker9@gmail.com>
2023-06-19 17:18:52 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace sayitahmin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {           
+            int tahmin = 0;
+            int hafiza;
+            int sayac = 0;
+            Random rastegele = new Random();
+            hafiza = rastegele.Next(1, 100);
+            while (tahmin != hafiza)
+            {
+                sayac++;
+                Console.Write("Lütfen bir sayı giriniz: ");
+                tahmin = Convert.ToInt16(Console.ReadLine());
+                if (tahmin > hafiza)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Daha küçük bir sayı giriniz.\n");                    
+                }
+                else if (tahmin < hafiza)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Daha büyük bir sayı giriniz.\n");                   
+                }
+            }           
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine();
+            Console.WriteLine("Tebriklerr...");
+            Console.WriteLine("{0}.denemede bildiniz :)", sayac);
+            Console.ReadLine();
+        }
+    }
+}
